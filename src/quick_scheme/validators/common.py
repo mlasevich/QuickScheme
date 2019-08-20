ded7cc49551fad550fa9e927f673a7356e747180
mlasevich <michael@legrig.com>
2019-08-20 11:00:20 -0700

    Update common.py
    Fix python2 compatibility

--- src/quick_scheme/validators/common.py
+++ src/quick_scheme/validators/common.py
@@ -4,8 +4,9 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-def failure(field_value, fmt, *args, level=logging.INFO):
+def failure(field_value, fmt, *args, **kwargs):
     ''' Print a Validation Failure Message '''
+    level = kwargs.get('level', logging.INFO)
     LOG.log(level, "Invalid Value for field '%s' in node '%s': %s",
             field_value.name(), field_value.node.quick_scheme.path_str(),
             fmt % args)
